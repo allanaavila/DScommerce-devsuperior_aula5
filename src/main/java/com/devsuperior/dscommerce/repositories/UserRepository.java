fd447e0a893dec6d9ad8c0a757530ebcb962460b
Allana Avila <allanavila@gmail.com>
2026-08-30 19:16:05 -0300

    configuracao do controle de acesso

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -6,6 +6,7 @@ import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,5 +19,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 				INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
 				WHERE tb_user.email = :email
 			""")
-    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+    List<UserDetailsProjection> searchUserAndRolesByEmail(@Param("email") String email);
 }
