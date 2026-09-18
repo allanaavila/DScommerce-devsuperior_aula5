175cb8c5bcbb3410af4e6c2fca05faff3c9ca35c
Allana Avila <allanavila@gmail.com>
2026-09-18 07:13:20 -0300

    Insert Image URL

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -7,14 +7,16 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
     public OrderItemDTO() {}
 
-    public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+    public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
         this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.imgUrl = imgUrl;
     }
 
     public OrderItemDTO(OrderItem entity) {
@@ -22,12 +24,14 @@ public class OrderItemDTO {
         name = entity.getProduct().getName();
         price = entity.getPrice();
         quantity = entity.getQuantity();
+        imgUrl = entity.getProduct().getImgUrl();
     }
 
     public Long getProductId() { return productId;}
     public String getName() { return name;}
     public Double getPrice() { return price;}
     public Integer getQuantity() { return quantity;}
+    public String getImgUrl() { return imgUrl;}
 
     public Double getSubTotal() {
         return price * quantity;
